56da099ce5b52716b5f7bd39068e0a7b41b2512e
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-08-31 22:39:27 +0300

    add private variables to the encrypt class.

---
+++ src/main/java/EnryptMe.java
@@ -0,0 +1,7 @@
+public class EnryptMe {
+    private static String text = "";
+    private static int shiftKey;
+    private static String cipherText = "";
+    private static String errorMessage = "";
+
+}
