87f2da4d1e72e455a39e5ccedcb3fc361e91185d
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-08-31 23:03:56 +0300

    add logic for error message using if statement.

--- src/test/java/EnryptMeTest.java
+++ src/test/java/EnryptMeTest.java
@@ -2,5 +2,11 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class EnryptMeTest {
+    @Test
+    public void encodeMethod_keyOutOfBoundError_String() {
+        EnryptMe testEncrypt = new EnryptMe();
+        String resultExpected = "key must be between 1 to 25";
+        assertEquals(resultExpected,testEncrypt.encodeMethod("here",27));
 
+    }
 }
