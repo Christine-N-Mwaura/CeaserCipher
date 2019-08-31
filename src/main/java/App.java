17130f55b35e7b258ef71884551255b524e5c644
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-08-31 23:43:36 +0300

    add while control structure to loop choices.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,17 @@
 import java.util.Scanner;
 
 public class App {
-    Scanner myScanner = new Scanner(System.in);
-    boolean ceaserCipherRunning = true;
-    EnryptMe encrypt = new EnryptMe();
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        boolean ceaserCipherRunning = true;
+        EnryptMe encrypt = new EnryptMe();
 
+        while (ceaserCipherRunning) {
+            System.out.println("Welcome to Ceaser Cipher program would you like to encrypt, decrypt or exit ?");
+
+            String choice = myScanner.nextLine();
+        }
+    }
 
 
 }
