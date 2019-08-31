d2380c839dade4e3c40b132c94b412b73b0f424c
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-08-31 22:09:56 +0300

    Make changes to the classes.

--- src/main/java/DecryptMe.java
+++ src/main/java/DecryptMe.java
@@ -1,4 +1,65 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class DecryptMe {
+    private static String encryptedText = "";
+    private static int shiftKey;
+    private static String decryptedText = "";
+    private static String errorMessage = "";
+
+    public  String decodeMethod(String encryptedText, int shiftKey) {
+        this.encryptedText = encryptedText;
+        if (shiftKey < 1 || shiftKey > 26) {
+            return errorMessage = "key must be between 1 to 25";
+        } else{
+
+
+            int length = encryptedText.length();
+            for (int i = 0; i < length; i++) {
+                char ch = encryptedText.charAt(i);
+                if (Character.isLetter(ch)) {
+                    if (Character.isUpperCase(ch)) {
+                        char c = (char) (ch - shiftKey);
+                        if (c < 'A') {
+                            decryptedText += (char) (ch + (26 - shiftKey));
+                        } else {
+                            decryptedText += c;
+                        }
+                    } else if (Character.isLowerCase(ch)) {
+                        char c = (char) (ch - shiftKey);
+                        if (c < 'a') {
+                            decryptedText += (char) (ch + (26 - shiftKey));
+                        } else {
+                            decryptedText += c;
+                        }
+                    }
+                } else {
+                    decryptedText += ch;
+
+                }
+            }
+        }
+        return  decryptedText;
+
+    }
+
+    public int getShiftKey()
+    {
+        return shiftKey;
+    }
+
+    public String getText()
+    {
+        return encryptedText;
+    }
+
+    public void setCipherText(String cipherText)
+    {
+        this. decryptedText = cipherText;
+    }
+
+    public void setErrorMessage(String errorMessage)
+    {
+        this.errorMessage = errorMessage;
+    }
+
 }
