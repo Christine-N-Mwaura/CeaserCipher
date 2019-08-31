b502f150f8d2a9dc8b369ce98bc3de42c4f8e1a6
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-08-31 23:40:37 +0300

    add main method with instance of encrypt class.

---
+++ src/main/java/App.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class App {
+    Scanner myScanner = new Scanner(System.in);
+    boolean ceaserCipherRunning = true;
+    EnryptMe encrypt = new EnryptMe();
+
+
+
+}
