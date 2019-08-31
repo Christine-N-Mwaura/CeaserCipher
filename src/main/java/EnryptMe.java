92ddda4bd12d2f5e0a2c05678dcf99da092951ab
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-08-31 22:43:07 +0300

    add getter and setter methods to the private variables

--- src/main/java/EnryptMe.java
+++ src/main/java/EnryptMe.java
@@ -4,4 +4,21 @@ public class EnryptMe {
     private static String cipherText = "";
     private static String errorMessage = "";
 
+    public int getShiftKey() {
+        return shiftKey;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setCipherText(String cipherText) {
+        this.cipherText = cipherText;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+
 }
